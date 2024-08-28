32614ec4b1a068069ee1f244a0f336cb0503164b
arron_xqslhoi <arron_xqslhoi@MSI>
2024-08-27 20:55:40 -0500

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//8/27/2024
+//Arron Stanley III
+//First of many
+//Data Structures
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	cout << "Hello Proffessor Ghosh!" << endl;
+
+	return 0;
+}
